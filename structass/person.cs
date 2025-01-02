852e543e0e7c341ddfba0adc2e80fc1e42b004f1
nagat1 <noga212008@outlook.com>
2025-01-01 22:00:04 -0800

    Add project files.

---
+++ structass/person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structass_
+{
+    //1.Define a struct "Person" with properties "Name" and "Age".
+    //Create an array of three "Person" objects and populate it with data.
+    //Then, write a C# program to display the details of all the persons in the array.
+
+	//3.Create a struct called "Person" with properties "Name" and "Age".
+	//Write a C# program that takes details of 3 persons as input from the user and
+	//displays the name and age of the oldest person.
+    internal struct Person
+    {
+		 
+        public string Name
+		{
+			get; set;
+		}
+
+		public int Age
+		{
+			get; set;
+		}
+		
+	
+
+	}
+}
---
+++ structass/point.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structass_
+{ //2.Create a struct called "Point" to represent a 2D point with properties "X" and "Y".
+  //Write a C# program that takes two points as input from the user
+  //and calculates the distance between them.
+    internal struct Point
+    {
+
+		int x;
+		int y;
+
+		public int X
+		{
+			get; set;
+		}	
+
+		public int Y
+		{
+			get; set;
+		}
+        public double distance(int x, int y, int xx, int yy)
+
+        {    
+            double d = Math.Sqrt(Math.Pow((xx - x), 2) + Math.Pow((yy - y), 2));
+			
+            return d;
+        }
+
+    }
+}
